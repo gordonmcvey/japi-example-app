8b8d24d67fb230ce6bfaa5fa3ce405eab759e406
gordonmcvey <grj.mcvey@gmail.com>
2025-07-01 22:46:01 +0100

    Improvements to DI config layout

---
+++ config/di/containerconfig.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+use gordonmcvey\exampleapp\controller\Health\Ping;
+use gordonmcvey\exampleapp\factory\DiControllerFactory;
+use gordonmcvey\exampleapp\middleware\ProcessedTime;
+use gordonmcvey\exampleapp\middleware\RequestMeta;
+use gordonmcvey\httpsupport\enum\factory\StatusCodeFactory;
+use gordonmcvey\httpsupport\request\payload\JsonPayloadHandler;
+use gordonmcvey\httpsupport\request\Request;
+use gordonmcvey\httpsupport\request\RequestInterface;
+use gordonmcvey\JAPI\Bootstrap;
+use gordonmcvey\JAPI\error\JsonErrorHandler;
+use gordonmcvey\JAPI\interface\controller\ControllerFactoryInterface;
+use gordonmcvey\JAPI\interface\error\ErrorHandlerInterface;
+use gordonmcvey\JAPI\interface\routing\RouterInterface;
+use gordonmcvey\JAPI\interface\routing\RoutingStrategyInterface;
+use gordonmcvey\JAPI\JAPI;
+use gordonmcvey\JAPI\middleware\CallStackFactory;
+use gordonmcvey\JAPI\routing\PathNamespaceStrategy;
+use gordonmcvey\JAPI\routing\Router;
+use Psr\Container\ContainerInterface;
+
+return [
+    // Attributes
+    "controllerRoot"                  => DI\string("gordonmcvey\\exampleapp\\controller"),
+    "jsonFlags"                       => DI\value(JSON_PRETTY_PRINT),
+    "exposeErrorDetails"              => DI\value(true),
+    // HTTP request
+    RequestInterface::class           => fn(ContainerInterface $container): Request
+        => Request::fromSuperGlobals($container->get(JsonPayloadHandler::class)),
+    // Error handlers
+    ErrorHandlerInterface::class      => DI\create(JsonErrorHandler::class)
+        ->constructor(
+            DI\get(StatusCodeFactory::class),
+            DI\get("jsonFlags"),
+            DI\get("exposeErrorDetails"),
+        ),
+    // Middleware
+    RequestMeta::class                => DI\create(RequestMeta::class)
+        ->constructor(DI\get("received")),
+    // Controllers
+    ControllerFactoryInterface::class => DI\get(DiControllerFactory::class),
+    Ping::class                       => DI\create(Ping::class)
+        ->method("addMiddleware", DI\get(ProcessedTime::class)),
+    // Routing
+    RoutingStrategyInterface::class   => DI\create(PathNamespaceStrategy::class)
+        ->constructor(DI\get("controllerRoot")),
+    RouterInterface::class            => DI\create(Router::class)
+        ->constructor(DI\get(RoutingStrategyInterface::class)),
+    // JAPI
+    Bootstrap::class                  => DI\create(Bootstrap::class)
+        ->constructor(
+            DI\get(RouterInterface::class),
+            DI\get(ControllerFactoryInterface::class),
+        ),
+    JAPI::class                       => DI\create(JAPI::class)
+        ->constructor(DI\get(CallStackFactory::class), DI\get(ErrorHandlerInterface::class))
+        ->method("addMiddleware", DI\get(RequestMeta::class)),
+];
