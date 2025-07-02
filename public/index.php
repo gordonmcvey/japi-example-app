4ad41d26bb8f3d97233c868749d7216507b31563
gordonmcvey <grj.mcvey@gmail.com>
2025-07-02 21:24:51 +0100

    Implemented DI using Symfony DI container

--- public/index.php
+++ public/index.php
@@ -4,22 +4,28 @@ declare(strict_types=1);
 
 $received = new DateTimeImmutable();
 
-use gordonmcvey\exampleapp\controller\health\Ping;
+use gordonmcvey\exampleapp\controller\Health\Ping;
+use gordonmcvey\exampleapp\factory\DiControllerFactory;
 use gordonmcvey\exampleapp\middleware\ProcessedTime;
 use gordonmcvey\exampleapp\middleware\RequestMeta;
 use gordonmcvey\httpsupport\enum\factory\StatusCodeFactory;
 use gordonmcvey\httpsupport\request\payload\JsonPayloadHandler;
+use gordonmcvey\httpsupport\request\payload\PayloadHandlerInterface;
 use gordonmcvey\httpsupport\request\Request;
-use gordonmcvey\httpsupport\request\RequestInterface;
-use gordonmcvey\JAPI\controller\ControllerFactory;
+use gordonmcvey\JAPI\Bootstrap;
 use gordonmcvey\JAPI\error\JsonErrorHandler;
 use gordonmcvey\JAPI\ErrorToException;
-use gordonmcvey\JAPI\interface\controller\RequestHandlerInterface;
+use gordonmcvey\JAPI\interface\controller\ControllerFactoryInterface;
+use gordonmcvey\JAPI\interface\error\ErrorHandlerInterface;
+use gordonmcvey\JAPI\interface\routing\RouterInterface;
+use gordonmcvey\JAPI\interface\routing\RoutingStrategyInterface;
 use gordonmcvey\JAPI\JAPI;
 use gordonmcvey\JAPI\middleware\CallStackFactory;
 use gordonmcvey\JAPI\routing\PathNamespaceStrategy;
 use gordonmcvey\JAPI\routing\Router;
 use gordonmcvey\JAPI\ShutdownHandler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -28,20 +34,51 @@ error_reporting(0);
 ini_set('display_errors', false);
 set_error_handler(new errorToException(), E_ERROR ^ E_USER_ERROR ^ E_COMPILE_ERROR);
 
-$errorHandler = new JsonErrorHandler(new StatusCodeFactory(), exposeDetails: true);
+$container = new ContainerBuilder();
+$container->set("received", $received);
+$container->setParameter("controllerRoot", "gordonmcvey\\exampleapp\\controller");
+$container->register(StatusCodeFactory::class, StatusCodeFactory::class);
+$container->register(ErrorHandlerInterface::class, JsonErrorHandler::class)
+    ->addArgument(new Reference(StatuscodeFactory::class))
+    ->addArgument(JSON_PRETTY_PRINT)
+    ->addArgument(true);
 
-register_shutdown_function(new ShutdownHandler($errorHandler));
+$container->register(ShutdownHandler::class, ShutdownHandler::class)
+    ->addArgument(new Reference(ErrorHandlerInterface::class));
 
-(new JAPI(new CallStackFactory(), $errorHandler))
-    ->addMiddleware(new RequestMeta($received))
+$container->register(RequestMeta::class, RequestMeta::class)
+    ->addArgument(new Reference("received"));
+
+$container->register(ProcessedTime::class, ProcessedTime::class);
+$container->register(Ping::class, Ping::class)
+    ->addMethodCall("addMiddleware", [new Reference(ProcessedTime::class)]);
+
+$container->register(CallstackFactory::class, CallstackFactory::class);
+$container->register(RoutingStrategyInterface::class, PathNamespaceStrategy::class)
+    ->addArgument("%controllerRoot%");
+
+$container->register(RouterInterface::class, Router::class)
+    ->addArgument(new Reference(RoutingStrategyInterface::class));
+
+$container->register(ControllerFactoryInterface::class, DiControllerFactory::class)
+    ->addArgument($container);
+
+$container->register(PayloadHandlerInterface::class, JsonPayloadHandler::class);
+
+$container->register(Bootstrap::class, Bootstrap::class)
+    ->addArgument(new Reference(RouterInterface::class))
+    ->addArgument(new Reference(ControllerFactoryInterface::class));
+
+$container->register(JAPI::class, JAPI::class)
+    ->addArgument(new Reference(CallStackFactory::class))
+    ->addArgument(new Reference(ErrorHandlerInterface::class))
+    ->addMethodCall("addMiddleware", [new Reference(RequestMeta::class)]);
+
+register_shutdown_function($container->get(ShutdownHandler::class));
+
+$container->get(JAPI::class)
     ->bootstrap(
-        function (RequestInterface $request): RequestHandlerInterface {
-            $router = new Router(new PathNamespaceStrategy("gordonmcvey\\exampleapp\\controller"));
-            $controller = (new ControllerFactory())->make($router->route($request));
-            $controller instanceof Ping && $controller->addMiddleware(new ProcessedTime());
-
-            return $controller;
-        },
-        Request::fromSuperGlobals(new JsonPayloadHandler()),
+        $container->get(Bootstrap::class),
+        Request::fromSuperGlobals($container->get(PayloadHandlerInterface::class)),
     )
 ;
