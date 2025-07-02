2339d0611caac25c585ad15efc6f0dd05c19a49a
gordonmcvey <grj.mcvey@gmail.com>
2025-07-02 21:24:51 +0100

    Implemented DI using Symfony DI container

--- public/index.php
+++ public/index.php
@@ -4,64 +4,81 @@ declare(strict_types=1);
 
 $received = new DateTimeImmutable();
 
-use Dotenv\Dotenv;
-use gordonmcvey\exampleapp\controller\health\Ping;
+use gordonmcvey\exampleapp\controller\Health\Ping;
+use gordonmcvey\exampleapp\factory\DiControllerFactory;
 use gordonmcvey\exampleapp\middleware\ProcessedTime;
 use gordonmcvey\exampleapp\middleware\RequestMeta;
 use gordonmcvey\httpsupport\enum\factory\StatusCodeFactory;
 use gordonmcvey\httpsupport\request\payload\JsonPayloadHandler;
+use gordonmcvey\httpsupport\request\payload\PayloadHandlerInterface;
 use gordonmcvey\httpsupport\request\Request;
-use gordonmcvey\httpsupport\request\RequestInterface;
-use gordonmcvey\httpsupport\response\sender\ResponseSender;
-use gordonmcvey\JAPI\controller\ControllerFactory;
+use gordonmcvey\JAPI\Bootstrap;
 use gordonmcvey\JAPI\error\JsonErrorHandler;
 use gordonmcvey\JAPI\ErrorToException;
-use gordonmcvey\JAPI\interface\controller\RequestHandlerInterface;
+use gordonmcvey\JAPI\interface\controller\ControllerFactoryInterface;
+use gordonmcvey\JAPI\interface\error\ErrorHandlerInterface;
+use gordonmcvey\JAPI\interface\routing\RouterInterface;
+use gordonmcvey\JAPI\interface\routing\RoutingStrategyInterface;
 use gordonmcvey\JAPI\JAPI;
 use gordonmcvey\JAPI\middleware\CallStackFactory;
 use gordonmcvey\JAPI\routing\PathNamespaceStrategy;
 use gordonmcvey\JAPI\routing\Router;
 use gordonmcvey\JAPI\ShutdownHandler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv->load();
-
-$dotenv->ifPresent("ERROR_REPORTING")->isInteger();
-error_reporting(((int) $_ENV["ERROR_REPORTING"]) ?? 0);
-
-$dotenv->ifPresent("DISPLAY_ERRORS");
-ini_set("display_errors", ((string) $_ENV["DISPLAY_ERRORS"]) ?? "");
-
-$dotenv->ifPresent("DISPLAY_STARTUP_ERRORS")->isBoolean();
-ini_set("display_startup_errors", (bool) $_ENV["DISPLAY_STARTUP_ERRORS"]);
-
+// For live you don't want any error output.  You might want to use different values here for local development/testing
+error_reporting(0);
+ini_set('display_errors', false);
 set_error_handler(new errorToException(), E_ERROR ^ E_USER_ERROR ^ E_COMPILE_ERROR);
 
-$dotenv->ifPresent("PRETTY_PRINT_JSON")->isBoolean();
-$dotenv->ifPresent("DETAILED_ERROR_OUTPUT")->isBoolean();
-$errorHandler = new JsonErrorHandler(
-    statusCodeFactory: new StatusCodeFactory(),
-    jsonFlags: ((bool) $_ENV["PRETTY_PRINT_JSON"]) ? JSON_PRETTY_PRINT : 0,
-    exposeDetails: (bool) $_ENV["DETAILED_ERROR_OUTPUT"],
-);
+$container = new ContainerBuilder();
+$container->set("received", $received);
+$container->setParameter("controllerRoot", "gordonmcvey\\exampleapp\\controller");
+$container->register(StatusCodeFactory::class, StatusCodeFactory::class);
+$container->register(ErrorHandlerInterface::class, JsonErrorHandler::class)
+    ->addArgument(new Reference(StatuscodeFactory::class))
+    ->addArgument(JSON_PRETTY_PRINT)
+    ->addArgument(true);
 
-$sender = new ResponseSender();
-register_shutdown_function(new ShutdownHandler($sender, $errorHandler));
+$container->register(ShutdownHandler::class, ShutdownHandler::class)
+    ->addArgument(new Reference(ErrorHandlerInterface::class));
 
-$dotenv->required("APP_CONTROLLER_NAMESPACE_ROOT");
+$container->register(RequestMeta::class, RequestMeta::class)
+    ->addArgument(new Reference("received"));
 
-(new JAPI(new CallStackFactory(), $errorHandler, $sender))
-    ->addMiddleware(new RequestMeta($received))
+$container->register(ProcessedTime::class, ProcessedTime::class);
+$container->register(Ping::class, Ping::class)
+    ->addMethodCall("addMiddleware", [new Reference(ProcessedTime::class)]);
+
+$container->register(CallstackFactory::class, CallstackFactory::class);
+$container->register(RoutingStrategyInterface::class, PathNamespaceStrategy::class)
+    ->addArgument("%controllerRoot%");
+
+$container->register(RouterInterface::class, Router::class)
+    ->addArgument(new Reference(RoutingStrategyInterface::class));
+
+$container->register(ControllerFactoryInterface::class, DiControllerFactory::class)
+    ->addArgument($container);
+
+$container->register(PayloadHandlerInterface::class, JsonPayloadHandler::class);
+
+$container->register(Bootstrap::class, Bootstrap::class)
+    ->addArgument(new Reference(RouterInterface::class))
+    ->addArgument(new Reference(ControllerFactoryInterface::class));
+
+$container->register(JAPI::class, JAPI::class)
+    ->addArgument(new Reference(CallStackFactory::class))
+    ->addArgument(new Reference(ErrorHandlerInterface::class))
+    ->addMethodCall("addMiddleware", [new Reference(RequestMeta::class)]);
+
+register_shutdown_function($container->get(ShutdownHandler::class));
+
+$container->get(JAPI::class)
     ->bootstrap(
-        function (RequestInterface $request): RequestHandlerInterface {
-            $router = new Router(new PathNamespaceStrategy($_ENV["APP_CONTROLLER_NAMESPACE_ROOT"]));
-            $controller = (new ControllerFactory())->make($router->route($request));
-            $controller instanceof Ping && $controller->addMiddleware(new ProcessedTime());
-
-            return $controller;
-        },
-        Request::fromSuperGlobals(new JsonPayloadHandler()),
+        $container->get(Bootstrap::class),
+        Request::fromSuperGlobals($container->get(PayloadHandlerInterface::class)),
     )
 ;
