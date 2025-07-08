f72b004586a0e486281363b020d440a42ba4f162
gordonmcvey <grj.mcvey@gmail.com>
2025-07-08 08:15:05 +0100

    Env support: Updated DI setup to make use of env for configuration

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 $received = new DateTimeImmutable();
 
 use DI\ContainerBuilder;
+use Dotenv\Dotenv;
 use gordonmcvey\httpsupport\request\RequestInterface;
 use gordonmcvey\JAPI\Bootstrap;
 use gordonmcvey\JAPI\ErrorToException;
@@ -13,16 +14,31 @@ use gordonmcvey\JAPI\ShutdownHandler;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// For live you don't want any error output.  You might want to use different values here for local development/testing
-error_reporting(0);
-ini_set('display_errors', false);
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$dotenv->required("APP_CONTROLLER_NAMESPACE_ROOT");
+$dotenv->ifPresent("DETAILED_ERROR_OUTPUT")->isBoolean();
+$dotenv->required("DI_COMPILE_DIR");
+$dotenv->ifPresent("DISPLAY_ERRORS");
+$dotenv->ifPresent("DISPLAY_STARTUP_ERRORS")->isBoolean();
+$dotenv->ifPresent("ERROR_REPORTING")->isInteger();
+$dotenv->ifPresent("PRETTY_PRINT_JSON")->isBoolean();
+
+error_reporting(((int) $_ENV["ERROR_REPORTING"]) ?? 0);
+ini_set("display_errors", ((string) $_ENV["DISPLAY_ERRORS"]) ?? "");
+ini_set("display_startup_errors", (bool) $_ENV["DISPLAY_STARTUP_ERRORS"]);
 set_error_handler(new errorToException(), E_ERROR ^ E_USER_ERROR ^ E_COMPILE_ERROR);
 
-$container = (new ContainerBuilder())
-    ->enableCompilation(__DIR__ . '/../.dicache')
+$builder = new ContainerBuilder();
+if ((bool) $_ENV["DI_COMPILE_DIR"]) {
+    $builder->enableCompilation($_ENV["DI_COMPILE_DIR"]);
+}
+$container = $builder
     ->addDefinitions(__DIR__ . '/../config/di/containerconfig.php')
     ->build()
 ;
+
 $container->set("received", $received);
 
 register_shutdown_function($container->get(ShutdownHandler::class));
